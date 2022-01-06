be45895fa597d02885a606a2cf3a8be95468ad94
schpa <chris.patrick.schreiner@gmail.com>
2022-01-06 21:39:10 +0100

    remove public

--- public/eykt/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { eykt.core.init_BANG_(); } catch (e) { console.error("An error occurred when calling (eykt.core/init!)"); throw(e); }
