e92b44615b09287162ddec1818229611595685f0
schpa <chris.patrick.schreiner@gmail.com>
2022-01-06 23:54:20 +0100

    Misc updateds

--- src/cljs/eykt/data.cljs
+++ src/cljs/eykt/data.cljs
@@ -2,22 +2,23 @@
   (:require [re-frame.core :refer [reg-sub]]
             [cljs.reader :refer [read-string]]))
 
+(def app-routes
+  [["/" {:name :r.common :header "Forsiden"}]
+   ["/baksiden" {:name :r.back :header "Baksiden"}]])
+
 (def screen-breakpoints
   {:breakpoints [:mobile 640 :tablet 992 :small-monitor 1200 :large-monitor],
    :debounce-ms 166})
-
-(def start-db
-  {:startup       true,
-   :app/show-help {1 false, 2 false, 5 false, 6 false, 9 true}})
-
-(def app-routes
-  [["/" {:name :r.common :header "Forsiden"}]])
 
 (reg-sub ::user-screenmode
          (fn [db]
            (get-in db [:settings :pref-screenmode] :auto)))
 
-;region
+;region initial-state
+
+(def start-db
+  {:startup       true,
+   :app/show-help {1 false, 2 false, 5 false, 6 false, 9 true}})
 
 (def localstorage-key "eykt-22")
 
--- src/cljs/eykt/spa.cljs
+++ src/cljs/eykt/spa.cljs
@@ -2,13 +2,22 @@
   (:require [re-frame.core :as rf]
             [kee-frame.router]
             [cljs.pprint :refer [pprint]]
-            [eykt.data :as data :refer [screen-breakpoints start-db app-routes]]))
+            [eykt.data :as data :refer [screen-breakpoints start-db app-routes]]
+            [kee-frame.core :as k]))
 
 (def route-table
   {:r.common (fn [_]
-               [:div
+               [:div.space-y-4.p-4
                 [:div.text-red-500.text-2xl "EYKT"]
-                [:pre.text-base.text-yellow-500.bg-black (with-out-str (pprint @re-frame.db/app-db))]])})
+                [:a {:href (k/path-for [:r.back])} "Baksiden"]
+                [:pre.text-base.text-yellow-500.bg-black.overflow-auto.-mx-4.p-2
+                 (with-out-str (pprint @re-frame.db/app-db))]])
+   :r.back   (fn [_]
+               [:div.space-y-4.p-4
+                [:div.text-red-500.text-2xl "BAKSIDEN"]
+                [:a {:href (k/path-for [:r.common])} "Forsiden"]
+                [:pre.text-base.text-yellow-500.bg-black.overflow-auto.-mx-4.p-2
+                 (with-out-str (pprint @re-frame.db/app-db))]])})
 
 (defn dispatch-main
   []
@@ -20,8 +29,6 @@
                       (kee-frame.router/make-route-component page @route))]
     [:div web-content]))
 
-
-
 (defn app-wrapper
   "takes care of light/dark-mode and loading-states"
   [content]
@@ -29,9 +36,6 @@
         html (aget (.getElementsByTagName js/document "html") 0)
         body (aget (.getElementsByTagName js/document "body") 0)]
     (.setAttribute html "class" (if (= :dark user-screenmode) "dark" ""))
-    ;(.setAttribute html "style" "height: 100%")
-    ;(.setAttribute body "style" "height: 100%")
-    ;{:class (get-in s/color-map [:main :content :bg])}
     (.setAttribute body
                    "class"
                    "font-sans inter bg-gray-100 dark:bg-gray-800 min-h-screen")
